873fd5d9043447350383880f6d0c7e63e9803d8a
Bartosz Adamczuk <bartosz.adamczuk@gmail.com>
2020-01-27 12:45:10 +0100

    Added footer
    Added footer and progress bar animation

---
+++ js/skills.js
@@ -0,0 +1,50 @@
+function getInfo(arr){
+    const skills =[
+        {
+            name:"HTML",
+            description :"HTML desc",
+            logo:"../img/htmlLogo.png"
+        },
+        {
+            name:"CSS",
+            description :"CSS desc"
+        },
+        {
+            name:"JAVASCRIPT",
+            description :"JS desc"
+        },
+        {
+            name:"REACT JS",
+            description :"React js desc"
+        },
+        {
+            name:"REACT NATIVE",
+            description :"React native desc"
+        },
+        {
+            name:"REDUX",
+            description :"Redux desc"
+        },
+        {
+            name:"GIT",
+            description :"Git desc"
+        }
+    ]
+    let container = document.getElementsByClassName("skillsInfoContainer")
+    container[0].style.opacity = 1
+    let name = document.getElementById("skillName")
+    let article = document.getElementById("skillsInfo")
+    skills.map((skill,i)=>{
+        if(arr === skill.name){
+            name.innerHTML = skills[i].name
+            article.innerHTML = skills[i].description
+        }
+    })
+}
+function resetInfo(){
+    // let name = document.getElementById("skillName")
+    // let article = document.getElementById("skillsInfo")
+    document.getElementsByClassName("skillsInfoContainer")[0].style.opacity = 0
+    // name.innerHTML = "Programing languages and technologies"
+    // article.innerHTML = "abc"
+}
